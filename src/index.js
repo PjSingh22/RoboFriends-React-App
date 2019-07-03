ea1e7da610af7bbdccbf76db4b024a655b0c2549
Prabhjot Singh <pjsingh1359@outlook.com>
2019-07-02 21:13:49 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
